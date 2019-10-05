a6884feed85e4697dffc9cc359804d3e971c8761
packaz123 <56036687+packaz123@users.noreply.github.com>
2019-10-05 19:36:55 +0700

    Add files via upload

---
+++ Exercise4_Pipat_V.py
@@ -0,0 +1,12 @@
+Foundation_English = 56
+General_Business = 65
+Introduction_to_Computer_Systems = 60
+Computer_Programming = 100
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Introduction_to_Computer_Systems)
+print("Computer Programming :",Computer_Programming)
+
+
+
